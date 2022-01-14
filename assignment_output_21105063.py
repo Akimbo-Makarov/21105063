2c1d19545c64db8968da419cefbaceeee2d7707a
Akimbo-Makarov <97727474+Akimbo-Makarov@users.noreply.github.com>
2022-01-14 18:38:56 +0530

    Create assignment_output_21105063.py

---
+++ assignment_output_21105063.py
@@ -0,0 +1,44 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==== RESTART: C:\Users\hitin\Documents\GitHub\21105063\assign_1_21105063.py ====
+Enter Your Number Here: 79
+79
+Enter Your Number Here: 80
+80
+Enter Your Number Here: 40
+40
+Your Average Is 66.33333333333333
+Enter Your Gross Income: 200000
+200000.0
+How Many Dependants Do You Have: 3
+3.0
+Deduction For Dpendants 9000.0
+Taxable Income:  181000.0
+Tax You Would Have To Pay:  40000.0
+State Your SID: 
+21105063
+State Your NAME: 
+Hitin Bhagat
+NOTE : gender should be one of the following 
+ 	M//F//U
+State Your Gender: 
+M
+State Your Course Name: 
+ECE
+State Your CGPA: 
+8.0
+[21105063, 'Hitin Bhagat', 'M', 'ECE', 8.0]
+First Student Marks: 
+89
+Second Student Marks: 
+80
+Third Student Marks: 
+90
+Fourth Student Marks: 
+50
+Fifth Student Marks: 
+44
+[44, 50, 80, 89, 90]
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Yellow']
