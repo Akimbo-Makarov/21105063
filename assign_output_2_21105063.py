91bebb8c1b63c98d865c132a6a9d474864e2670a
Akimbo-Makarov <97727474+Akimbo-Makarov@users.noreply.github.com>
2022-01-26 23:46:43 +0530

    Assignment 2

---
+++ assign_output_2_21105063.py
@@ -0,0 +1,97 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============= RESTART: E:\coding\assignment\aasign_2_21105063py.py =============
+Ques.1}
+
+
+Type Your String: python is a case sensitive language 
+
+a. Lenght of the entered string is: 36 
+
+b. Reversed Order of String:  egaugnal evitisnes esac a si nohtyp 
+
+c. New String is: a case sensitive 
+
+d. Repalced String is: python is object oriented language  
+
+e. Index of Substring 'a' is: 10 
+
+Your String with no spaces pythonisacasesensitivelanguage
+
+
+
+
+
+Ques.2}
+
+
+Enter Your Name: Hitin Bhagat
+Enter Your SID: 21105063
+Enter Your Department: ECE
+Enter Your CGPA: 8.7
+Hey, Hitin Bhagat Here!
+My SID is 21105063
+I am from ECE Department and My CGPA is 8.700000
+
+
+
+
+
+Ques.3}
+
+
+a =  56 
+ b =  10
+a. a&b =  8 
+
+b. a|b =  58 
+
+c. a^b =  50 
+
+d. Left Shift of 'a' by 2 bits =  224 
+   Left Shift of 'b' by 2 bits =  40
+e. Right Shift of 'a' by 2 bits =  14 
+   Right Shift of 'b' by 4 bits =  0
+
+
+
+
+
+Ques.4}
+
+
+Enter three numbers to find the greatest number:-
+First Number = 58
+Second Number = 80
+Third Number = 67
+
+Greatest Number is: 80.0
+
+
+
+
+
+Ques.5}
+
+
+Enter a sentence to know wheather it contain 'name'or not:-
+
+Enter Your Sentence: my name is Hitin Bhagat
+
+ Does this sentence contain word 'name' : Yes
+
+
+
+
+
+Ques.6}
+
+
+Enter three lengths to se wheather a triangle can be formed from these lenghts or not:-
+
+Length of First Side of Triangle = 3
+Length of Second Side of Trisngle = 4
+Length of Third Side of Triangle = 5
+
+Yes, Triangle can be formed.
