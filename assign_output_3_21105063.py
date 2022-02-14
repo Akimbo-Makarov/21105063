a04b09c3a97512ec13aaa101a8341d831d99f23c
Akimbo-Makarov <97727474+Akimbo-Makarov@users.noreply.github.com>
2022-02-14 23:51:34 +0530

    3

---
+++ assign_output_3_21105063.py
@@ -0,0 +1,71 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==== RESTART: C:\Users\hitin\Documents\GitHub\21105063\assign_3_21105063.py ====
+           Q 1
+
+Enter string:
+fgbhjngvfbcgvbhnjgnfthgyhujmnmgfhjnbgvfcthyj
+Number of occurence of f is 5
+Number of occurence of g is 7
+Number of occurence of b is 4
+Number of occurence of h is 6
+Number of occurence of j is 5
+Number of occurence of n is 5
+Number of occurence of v is 3
+Number of occurence of c is 2
+Number of occurence of t is 2
+Number of occurence of y is 2
+Number of occurence of u is 1
+Number of occurence of m is 2
+          Q 2
+
+Enter date in dd/mm/yyyy format(eg. 7/1/2020): 21/09/2003
+The next date is 22 / 9 / 2003
+                     Q 3
+
+Number of elements: 4
+enter the element: 1
+enter the element: 3
+enter the element: 6
+enter the element: 8
+[(1, 1), (3, 9), (6, 36), (8, 64)]
+                     Q 4
+
+Enter Grade: 5
+Your grade is C and Below Average Performance.
+                      Q 5
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+                      Q 6
+
+Enter Name: Hitin Bhagat
+Enter SID: 21105063
+Do you want to enter name and SID <y/n>: y
+Enter Name: H
+Enter SID: 68712783
+Do you want to enter name and SID <y/n>: y
+Enter Name: hdhd
+Enter SID: 61238727
+Do you want to enter name and SID <y/n>: n
+A. The dictionary is:
+ {21105063: 'Hitin Bhagat', 68712783: 'H', 61238727: 'hdhd'}
+B. After sorting:
+ {21105063: 'Hitin Bhagat', 61238727: 'hdhd', 68712783: 'H'}
+Enter SID: 21105063
+C. Name of student with SID 21105063 is:  Hitin Bhagat
+                                  Q 7
+Enter the number of elements in fibonacci sequence: 15
+The fibonacci sequence is:  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
+The average of the sequence= 65.73333333333333
+                                   Q 8
+A. {1, 3, 5, 6, 8}
+B. {17, 3, 6, 8, 9, 13}
+C. {1, 2, 4, 5}
+D. {6, 7, 8, 9, 10}
+E. {10, 7}
